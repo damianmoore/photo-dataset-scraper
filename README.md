c46fe704c827c470ded6a6e4a978bb327705a5f3
Damian Moore <damian@epixstudios.co.uk>
2017-06-07 00:55:14 +0100

    Download images script, updated readme

--- README.md
+++ README.md
@@ -2,12 +2,42 @@
 
 Tools to create a labelled dataset of photos for deep learning image classification.
 
+
+## Requirements
+
 First install the Python required packages:
 
     pip install -r requirements.txt
 
 You'll need a Flickr API key and secret that should be entered in `settings.py`.
 
-Get the most "intersting" photos from Flickr and obtain the tags that were assigned to them. We'll keep track of how many times each tag was used and create a spreadsheet `tags.csv` of them all ordered by frequency.
+
+## Identifying popular tags
+
+We'll get the most "intersting" photos from Flickr and obtain the tags that were assigned to them. We keep track of how many times each tag was used and create a spreadsheet `tags.csv` of them all ordered by frequency.
 
     ./collect_tags.py
+
+You'll want to refine this list of tags by deleting rows that are not suitable. Here are some types of tag I removed:
+
+  * **Brands and models of camera** (don't really describe what's in the photo)
+  * **Large geographic areas like countries and continents** (I think photos of these are too varied and overlap too much to be identifiable)
+  * **Different languages** (you can do translation later on in UIs and search indexes - for now just use one language)
+  * **Synonyms** (there's no need to collect duplicate data and have labels strongly competing with each other - we can store these to improve the search functionality when we build it later)
+  * **Terms that are too broad**
+
+
+## Downloading images
+
+Next we download a set number of images for each of the tags in `tags.csv` and save them in labelled directories.
+
+    ./download_tagged_images.py
+
+**Warning:** You will be downloading lots of user-generated images that are very diverse. Because of this, there will undoubtably be some images that are unsuitable for children and unsuitable for work environments.
+
+
+## Training
+
+You may want to sort through and delete some of the collected images by hand to improve the accuracy of your training. I aim to build a web-based tool for doing this more quickly in future. You could also croud-source this step using something like Amazon's Mechanical Turk.
+
+Once you have built and refined your dataset you might want to use my simple wrapper for training an image classifier model: https://github.com/damianmoore/tensorflow-image-classifier
---
+++ download_tagged_images.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import argparse
+from collections import defaultdict
+import csv
+import hashlib
+import multiprocessing
+import os
+import urllib
+
+import flickrapi
+from skimage import io
+
+from settings import FLICKR_API_KEY, FLICKR_API_SECRET
+
+
+MISSING_IMAGE_SHA1 = '6a92790b1c2a301c6e7ddef645dca1f53ea97ac2'
+IMAGES_PER_TAG = 2000
+IMAGES_PER_PAGE = 500
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+training_dirname = os.path.join(dirname, 'data')
+
+
+def download_image(args_tuple):
+    "For use with multiprocessing map. Returns filename on fail."
+    try:
+        url, filename = args_tuple
+        if not os.path.exists(filename):
+            urllib.urlretrieve(url, filename)
+        with open(filename) as f:
+            assert hashlib.sha1(f.read()).hexdigest() != MISSING_IMAGE_SHA1
+        io.imread(filename)  # Test read the image
+        return True
+    except KeyboardInterrupt:
+        raise Exception()  # multiprocessing doesn't catch keyboard exceptions
+    except:
+        return False
+
+
+def get_tags_from_csv():
+    tags = []
+    with open('tags.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            tags.append(row[0])
+    return tags
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Download images from Flickr searches to labelled directory')
+    parser.add_argument(
+        '-i', '--images', type=int, default=-1,
+        help="number of images to use (-1 for all [default])",
+    )
+    parser.add_argument(
+        '-w', '--workers', type=int, default=-1,
+        help="num workers used to download images. -x uses (all - x) cores [-1 default]."
+    )
+    parser.add_argument(
+        '-l', '--labels', type=int, default=99999,
+        help="if set to a positive value, only sample images from the first number of labels."
+    )
+
+    args = parser.parse_args()
+
+    flickr = flickrapi.FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET, format='parsed-json')
+
+    urls = []
+    page = 1
+    tags = defaultdict(int)
+
+    try:
+        os.mkdir(training_dirname)
+    except FileExistsError:
+        pass
+
+    for tag in get_tags_from_csv()[:args.labels]:
+        while True:
+            photos = flickr.photos.search(sort='interestingness-desc', per_page=IMAGES_PER_PAGE, page=page, content_type=1, text=tag)['photos']['photo']
+
+            for photo in photos:
+                try:
+                    os.mkdir(os.path.join(training_dirname, tag))
+                except OSError:
+                    pass
+                url = 'https://farm{}.staticflickr.com/{}/{}_{}.jpg'.format(photo.get('farm'), photo.get('server'), photo.get('id'), photo.get('secret'))
+                urls.append((url, os.path.join(training_dirname, tag, '{}.jpg'.format(photo.get('id')))))
+
+                if len(urls) >= IMAGES_PER_TAG:
+                    break
+
+            if len(urls) >= IMAGES_PER_TAG:
+                break
+            page += 1
+
+        num_workers = args.workers
+        if num_workers <= 0:
+            num_workers = multiprocessing.cpu_count() + num_workers
+
+        print('Downloading {} images tagged as \'{}\' using {} workers...'.format(len(urls), tag, num_workers))
+        pool = multiprocessing.Pool(processes=num_workers)
+        map_args = urls
+        results = pool.map(download_image, map_args)
+
+        urls = []
+        page = 1
